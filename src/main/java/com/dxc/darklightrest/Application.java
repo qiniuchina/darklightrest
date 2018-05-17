a5c20985b7735a38afd803318cc5ac78a2680461
luoxiao24 <772789856@qq.com>
2018-05-17 11:20:18 +0800

    luoxiao： swagger

--- src/main/java/com/dxc/darklightrest/Application.java
+++ src/main/java/com/dxc/darklightrest/Application.java
@@ -1,8 +1,11 @@
-package com.dxc;
+package com.dxc.darklightrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
