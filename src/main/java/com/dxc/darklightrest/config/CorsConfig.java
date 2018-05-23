1eac0a0763d9d0e2b75ec4944fbab7410fe107b4
luoxiao24 <772789856@qq.com>
2018-05-23 15:53:50 +0800

    luoxiao

---
+++ src/main/java/com/dxc/darklightrest/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.dxc.darklightrest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")  
+        .allowCredentials(true)  
+        .allowedHeaders("*")  
+        .allowedOrigins("*")  
+        .allowedMethods("*");
+	}
+
+}
